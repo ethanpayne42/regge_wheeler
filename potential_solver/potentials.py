e2f809c34ca2b105a90fa4810c2ff00e170ea49c
Ethan Payne <epay1@student.monash.edu>
2018-08-26 23:08:31 +1000

    Make toy ECO model which can be evaluated

--- potential_solver/potentials.py
+++ potential_solver/potentials.py
@@ -13,13 +13,19 @@ def BH_tort(rs,M):
     return 2*M*(1+np.real(lambertw(np.exp(rs/(2*M)-1))))
 
 def BH_potential(rs,M, l=2):
-    r = BH_tort(rs+shifter(2.001,M),M)
+    r0 = 2.000001*M
+    shift = 0.+shifter(r0,M)
+    r = BH_tort(rs+shift,M)
 
-    return (1-(2*M)/r)*(l*(l+1)/r**2-6*M/r**3)
+    print(r.min())
+
+    return (1-2*M/r)*(l*(l+1)/r**2-6*M/r**3)
 
 def WH_tort(rs,M):
 
-    shift = -shifter(2.001*M,M)
+    r0 = 2.000001*M
+
+    shift = -shifter(r0,M)
 
     return np.piecewise(rs, [rs < 0, rs >= 0],
     [lambda rs: BH_tort(-rs-shift,M),
@@ -34,25 +40,32 @@ def WH_potential(rs, M, l=2):
 
 def ECO_mass(r, M):
     # Piecewise mass function for the ECO case, where we have some radial mass function for rs < 0
-    r0 = 2.001*M
+    r0 = 2.000001*M
 
     return np.piecewise(r, [r < r0, r >= r0],
     [lambda r: M * (r/r0)**3,
     lambda r: M])
 
 def ECO_inner(rs,M):
-    r0 = 2.001*M
+    r0 = 2.000001*M
     numer = r0**(1.5)*np.tanh(np.sqrt(2)*\
             np.sqrt(M)/r0**(1.5)*(rs+20)) # TODO weird shift
     denom = np.sqrt(2*M)
 
     r_inner = numer/denom
 
+    # return np.piecewise(rs, [rs > -18, rs <= -18.],
+    #                     [lambda rs: r0**(1.5)*np.tanh(np.sqrt(2)*\
+    #                             np.sqrt(M)/r0**(1.5)*(rs+20))/np.sqrt(2*M),
+    #                     lambda rs: 2*M)
+
     return r_inner
+
+
 
 def ECO_tort(rs, M):
     # Function to convert r* to r in ECO coordinates
-    r0 = 2.001*M
+    r0 = 2.000001*M
 
     shift = -shifter(r0,M)
 
@@ -66,16 +79,15 @@ def ECO_potential(rs, M, l=2):
 
     r = ECO_tort(rs,M)
 
-    r0 = 2.001*M
+    r0 = 2.000001*M
 
-    return np.piecewise(r, [rs < 0, rs>= 0,r < ECO_tort(-20,M)], #TODO clean up
-    [lambda r: (1-2*M*r**2/r0**3)*(l*(l+1)/r**2-6*M/r0**3),
-    lambda r: (1-(2*M)/r)*(l*(l+1)/r**2-6*M/r**3),
-    lambda r: 1e9])
+    return np.piecewise(rs, [rs < 0, rs>= 0], #TODO clean up
+    [lambda rs: 10*np.exp(-(rs+20)**2/4),#(1-2*M*r**2/r0**3)*(l*(l+1)/r**2-6*M/r0**3),
+    lambda rs: (1-(2*M)/ECO_tort(rs,M))*(l*(l+1)/ECO_tort(rs,M)**2-6*M/ECO_tort(rs,M)**3)])
 
 def TS_tort(rs, M):
     # Function to convert r* to r in TS coordinates
-    r0 = 2.001*M
+    r0 = 2.000001*M
 
     shift = -shifter(r0,M)
 
@@ -89,24 +101,30 @@ def TS_potential(rs, M, l=2):
 
     r = TS_tort(rs,M)
 
-    r0 = 2.001*M
+    r0 = 2.000001*M
 
     return np.piecewise(r, [rs < 0, rs>= 0,r < TS_tort(-0.1,M)], #TODO clean up
     [lambda r: (l*(l+1)/r**2),
     lambda r: (1-(2*M)/r)*(l*(l+1)/r**2-6*M/r**3),
-    lambda r: 1e9])
+    lambda r: 10])
 
 if __name__ == '__main__':
     M = 1
+    L = 2
 
-    xs = np.arange(-200,200,1e-1)*M
+    xs = np.arange(-200,200.01,1e-1)*M
 
     print(len(xs))
 
-    BH_pot = BH_potential(xs,M)
-    WH_pot = WH_potential(xs,M)
-    ECO_pot = ECO_potential(xs,M)
-    TS_pot = TS_potential(xs,M)
+    eco_tort = ECO_tort(xs,M)
+
+    BH_pot = BH_potential(xs,M,l=L)
+    WH_pot = WH_potential(xs,M,l=L)
+    ECO_pot = ECO_potential(xs,M,l=L)
+    TS_pot = TS_potential(xs,M,l=L)
+
+    plt.plot(xs,ECO_pot)
+    plt.show()
 
     potentials = np.array([xs, BH_pot, WH_pot,
                            ECO_pot, TS_pot]).T
@@ -118,6 +136,6 @@ if __name__ == '__main__':
     plt.plot(xs/M,ECO_pot*M**2,linestyle='--',label='ECO')
     plt.plot(xs/M,TS_pot*M**2,label='TS')
     plt.xlim(min(xs),max(xs))
-    plt.ylim(0,0.16)
+    plt.ylim(-0.16,0.16)
     plt.legend()
     plt.show()
